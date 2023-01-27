ec2941c7f25139533b262cc3ac41a03c688a058c
askiefer <annaskiefer@yahoo.com>
2023-01-27 10:31:54 -0800

    Added initial pass at assembly ai model

---
+++ assemblyai.py
@@ -0,0 +1,110 @@
+import argparse
+import os
+import time
+import utils
+import csv
+import pandas as pd
+from common import get_texts, CSV_FILEPATH
+
+
+NUM_VIDEOS = 20
+DIRPATH = "/Users/annas.kiefer/Desktop/transcribe"
+FILEPATH = f"{DIRPATH}/DD_transcripts_serota.csv"
+
+
+def run_assemblyai_model(video_filepath: str, video_id: str, start_time: int, end_time: int):
+
+    filepath_out = f"{DIRPATH}/assembly/{video_id}_assembly.txt"
+
+    api_key = os.getenv("AAI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("AAI_API_KEY environment variable not set. Try setting it now, or passing in your "
+                           "API key as a command line argument with `--api_key`.")
+
+    # Create header with authorization along with content-type
+    header = {
+        'authorization': api_key,
+        'content-type': 'application/json'
+    }
+
+    # don't need to upload file
+    # upload_url = utils.upload_file(video_filepath, header)
+
+    start_run_time = time.time()
+    # Request a transcription
+    transcript_response = utils.request_transcript(video_filepath, header, start_time, end_time)
+
+    # Create a polling endpoint that will let us check when the transcription is complete
+    polling_endpoint = utils.make_polling_endpoint(transcript_response)
+    print(f"{video_id}", polling_endpoint)
+
+    # Wait until the transcription is complete
+    utils.wait_for_completion(polling_endpoint, header)
+    end_run_time = time.time()
+
+    # Request the paragraphs of the transcript
+    # paragraphs = utils.get_paragraphs(polling_endpoint, header)
+    response = utils.get_sentences(polling_endpoint, header)
+
+    print(f"{video_id} Transcription time: {(end_run_time - start_run_time)}")
+
+    # Save and print transcript
+    with open(filepath_out, 'w') as f:
+        for r in response:
+            f.write(r['text'] + '\n')
+
+    return
+
+
+def sort_videos(videos):
+    videos_with_full_time = []
+    for vid_id, info in videos.items():
+        vid_start_time = info["start_time"]
+        vid_end_time = info["end_time"]
+        full_time = vid_end_time - vid_start_time
+        info["full_time"] = full_time
+        info["video_id"] = vid_id
+        videos_with_full_time.append(info)
+
+    vids = sorted(videos_with_full_time, key=lambda x: x["full_time"], reverse=True)
+    return vids
+
+def main():
+
+    with open(CSV_FILEPATH) as f:
+        reader = csv.DictReader(f)
+
+        videos = {}
+
+        for row in reader:
+            hearing_date = row["Hearing date"]
+
+            if hearing_date[-2:] == "18":  # only get videos from 2018 hearing
+                video_link = row["Link to video"]
+                video_id = os.path.basename(video_link).split(".mp4")[0]
+                full_video_link = video_link.split("#t=")[0]
+                if video_id not in videos:
+                    start_time = int(row["Start"])
+                    videos[video_id] = {"video_link": full_video_link, "start_time": start_time}
+                end_time = int(row["End"])
+                videos[video_id]["end_time"] = end_time
+
+        videos = sort_videos(videos)
+
+        video_count = 0
+        for info in videos:
+            if video_count <= NUM_VIDEOS:
+                vid_link = info["video_link"]
+                # convert end_time from s to ms
+                vid_start_time = info["start_time"] * 1000
+                vid_end_time = info["end_time"] * 1000
+                vid_id = info["video_id"]
+                # print(vid_id, vid_link, vid_start_time, vid_end_time)
+                run_assemblyai_model(vid_link, vid_id, vid_start_time, vid_end_time)
+                video_count += 1
+        #         break
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ transcribe.py
@@ -0,0 +1,111 @@
+import csv
+import os
+import pandas as pd
+import spacy
+import re
+import string
+# from assemblyai import run_assemblyai_model
+# from deepgram import run_deepgram_model
+
+
+DIRPATH = "/Users/annas.kiefer/Desktop/transcribe"
+FILEPATH = f"{DIRPATH}/DD_transcripts_serota.csv"
+
+
+# nlp = spacy.load('en_core_web_lg')
+
+
+# AssemblyAI links to full text
+# 7f5uDPxTBXo https://www.assemblyai.com/playground/transcript/rj9268sd9s-cb9d-4102-9fe2-ff2ed2d4c300
+
+
+def pipeline():
+	with open(FILEPATH) as f:
+		reader = csv.DictReader(f)
+		for row in reader:
+			video_link = row["Link to video"]
+			full_video_link = video_link.split("#t=")[0]
+			video_id = os.path.basename(video_link).split(".mp4")[0]
+
+			# assemblyai_file = f"{DIRPATH}/assembly/{vid_id}_assembly.txt"
+			# deepgram_file = f"{DIRPATH}/deepgram/{vid_id}_deepgram.txt"
+			
+			run_assemblyai_model(full_video_link, video_id, assemblyai_file)
+			# run_deepgram_model(full_video_link, video_id, deepgram_file)
+
+
+def main():
+	texts = get_groundtruth_text()
+	breakpoint()
+	for vid_id, truth in texts.items():
+
+		groundtruth_file = f"{DIRPATH}/groundtruth/{vid_id}_truth.txt"
+		# assemblyai_file = f"{DIRPATH}/assembly/{vid_id}_assembly.txt"
+		# deepgram_file = f"{DIRPATH}/deepgram/{vid_id}_deepgram.txt"
+
+		# add space after any punctuation
+		re.sub(r'\.(?!\s|\d|$)', '. ', truth)
+		# save the groundtruth data to open later
+		if not os.path.exists(groundtruth_file):
+			with open(groundtruth_file, "w") as f:
+				f.write(truth)
+				f.close()
+		
+		# with open(assemblyai_file, "r") as f:
+		# 	assemblyai = f.read().replace('\n', '')
+
+		# with open(deepgram_file, "r") as f:
+		# 	deepgram = f.read().replace('\n', '')
+
+		# metrics(vid_id, truth, assemblyai, deepgram)
+		break
+
+
+def metrics(video_id: str, truth: str, assemblyai: str, deepgram: str):
+
+	print("\nGroundtruth:")
+	print(truth)
+	print("\nAssemblyAI:")
+	print(assemblyai)
+	print("\nDeepgram:")
+	print(deepgram)
+
+	print("\n")
+
+	# sentence to sentence comparison
+	# diarization
+
+	cosine_similarity_assembly = cosine_similarity(video_id, truth, assemblyai, "assemblyai")
+	cosine_similarity_deepgram = cosine_similarity(video_id, truth, deepgram, "deepgram")
+
+
+def cosine_similarity(video_id: str, truth: str, test: str, model: str):
+
+	truth_text = nlp(truth)
+	test_text = nlp(test)
+
+	# truth_text_no_puncuation = truth.translate(str.maketrans('', '', string.punctuation))
+	# assemblyai_no_punctuation = assemblyai.translate(str.maketrans('', '', string.punctuation))
+
+	truth_text_no_stop_words = nlp(' '.join([str(t) for t in truth_text if not t.is_stop]))
+	test_text_no_stop_words = nlp(' '.join([str(t) for t in test_text if not t.is_stop]))
+
+	similarity = truth_text.similarity(test_text)
+	similarity_no_stop_words = truth_text_no_stop_words.similarity(test_text_no_stop_words)
+
+	print(f"Video ID: {video_id}: ")
+	print(f"   Cosine similarity {model}: {similarity}")
+	print(f"   Cosine similarity {model} no stop words: {similarity_no_stop_words}")
+
+
+if __name__ == "__main__":
+    # read_csv()
+    # main()
+    get_texts()
+    # Steps
+    # count how many bills are in 2018
+    # Run assembly ai model on 2018 
+
+
+
+
